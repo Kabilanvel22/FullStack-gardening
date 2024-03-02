7b34719b4a53f307c73cb69e46d8fecb56b5e8dc
Kabilan V <120373629+Kabilanvel22@users.noreply.github.com>
2024-03-02 11:05:00 +0530

    Delete back-end/fullstackproject/demo directory

--- back-end/fullstackproject/demo/src/main/java/com/repository/GardenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.entity.Garden;
-
-public interface GardenRepository extends JpaRepository<Garden, Integer>{
-	
-}
--- back-end/fullstackproject/demo/src/main/java/com/repository/PlantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.entity.Plant;
-
-public interface PlantRepository extends JpaRepository<Plant, Long>{
-	Plant findByplantId(long id);
-}
